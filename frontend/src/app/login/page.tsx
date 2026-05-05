582e45c1b53068ec5d193d7197168e636cd5148e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-05 08:54:50 -0300

    feat: adds page.tsx to frontend/src/app/login/

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/ui/LoginForm";
+
+function Login() {
+  return (
+    <section className="py-16 flex items-center justify-center">
+      <LoginForm />
+    </section>
+  );
+}
+
+export default Login;
