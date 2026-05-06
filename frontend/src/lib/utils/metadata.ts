47bd7760145a69cfe75fded0af461dc85c6dd6e0
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-06 11:59:55 -0300

    feat: adds metadata.ts to frontend/src/lib/utils/

---
+++ frontend/src/lib/utils/metadata.ts
@@ -0,0 +1,15 @@
+import { getTranslations } from "next-intl/server";
+
+type MetadataParams = {
+  locale: string;
+  namespace: string;
+};
+
+export async function getPageMetadata({ locale, namespace }: MetadataParams) {
+  const t = await getTranslations({ locale, namespace });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
