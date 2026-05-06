20a0e0f67efddde9d8daf9e9503fb3020b9d5411
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-06 11:23:13 -0300

    feat: relocates page.tsx to frontend/src/app/[locale]/works/

---
+++ frontend/src/app/[locale]/works/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Experiencia | Emmanuel Cruz",
+  description: "Mis trabajos realizados",
+};
+
+function WorksPage() {
+  return (
+    <div>
+      <h1>Works</h1>
+    </div>
+  );
+}
+
+export default WorksPage;
