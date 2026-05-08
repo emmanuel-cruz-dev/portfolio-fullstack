8e0045e54ef7f8ac6c2d6d714f804b6e4cbb4055
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-08 13:28:30 -0300

    feat: adds stackData.ts to frontend/src/constants/

---
+++ frontend/src/constants/stackData.ts
@@ -0,0 +1,18 @@
+export const INNER_STACK = [
+  { src: "/icons/nextjs.png", alt: "Next.js" },
+  { src: "/icons/react.png", alt: "React" },
+  { src: "/icons/angular.png", alt: "Angular" },
+  { src: "/icons/fastapi.png", alt: "FastAPI" },
+  { src: "/icons/spring-boot.png", alt: "Spring Boot" },
+  { src: "/icons/tailwind.png", alt: "Tailwind" },
+  { src: "/icons/nestjs.png", alt: "NestJS" },
+];
+
+export const OUTER_STACK = [
+  { src: "/icons/css.png", alt: "CSS" },
+  { src: "/icons/html.png", alt: "HTML" },
+  { src: "/icons/javascript.png", alt: "JavaScript" },
+  { src: "/icons/python.png", alt: "Python" },
+  { src: "/icons/java.png", alt: "Java" },
+  { src: "/icons/typescript.png", alt: "TypeScript" },
+];
