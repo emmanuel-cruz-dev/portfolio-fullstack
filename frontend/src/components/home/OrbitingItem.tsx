40f8cc47a3426108250a425a84e6521ff20a3acf
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-08 13:30:25 -0300

    feat: adds OrbitingItem.tsx to frontend/src/components/home/

---
+++ frontend/src/components/home/OrbitingItem.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+import { OrbitingCircles } from "../ui";
+import { INNER_STACK, OUTER_STACK } from "@/constants";
+
+function OrbitingItem() {
+  const renderIcons = (
+    stack: {
+      src: string;
+      alt: string;
+    }[]
+  ) =>
+    stack.map((icon) => (
+      <Image
+        key={icon.alt}
+        src={icon.src}
+        alt={icon.alt}
+        width={32}
+        height={32}
+        className="size-8"
+      />
+    ));
+  return (
+    <article className="relative flex items-center justify-center h-100 w-full overflow-hidden">
+      <OrbitingCircles>{renderIcons(INNER_STACK)}</OrbitingCircles>
+      <OrbitingCircles radius={100} reverse>
+        {renderIcons(OUTER_STACK)}
+      </OrbitingCircles>
+    </article>
+  );
+}
+
+export default OrbitingItem;
