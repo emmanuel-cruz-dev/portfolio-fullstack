8442b463c4e47fd7b9a0630a228b304df478e294
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-08 09:48:52 -0300

    fix: adds getIsDark to frontend/src/hooks/useIsDarkTheme.ts

--- frontend/src/hooks/useIsDarkTheme.ts
+++ frontend/src/hooks/useIsDarkTheme.ts
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 
+const getIsDark = () =>
+  typeof window !== "undefined" &&
+  document.documentElement.classList.contains("dark");
+
 export const useIsDarkTheme = () => {
-  const [isDark, setIsDark] = useState(() =>
-    document.documentElement.classList.contains("dark")
-  );
+  const [isDark, setIsDark] = useState(getIsDark);
 
   useEffect(() => {
     const observer = new MutationObserver(() => {
