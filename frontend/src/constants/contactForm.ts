5b984d70353d64cca7a9ae32911e51f62bb059b1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-12 15:13:28 -0300

    feat: adds contactForm.ts to frontend/src/constants/

---
+++ frontend/src/constants/contactForm.ts
@@ -0,0 +1,2 @@
+export const RATE_LIMIT_KEY = "contact_submissions";
+export const MAX_PER_DAY = 3;
--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from "./contactData";
+export * from "./contactForm";
 export * from "./stackData";
 export * from "./emailJs";
