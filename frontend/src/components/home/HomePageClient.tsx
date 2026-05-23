65da4983f6eaa2d3487f20db666b7fd725bfda3f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-05-23 14:24:23 -0300

    feat: adds HomePageClient.tsx to frontend/src/components/home/

---
+++ frontend/src/components/home/HomePageClient.tsx
@@ -0,0 +1,19 @@
+import {
+  HeroHome,
+  StatsSection,
+  AboutSection,
+  SkillsSection,
+} from "@/components";
+
+function HomePageClient() {
+  return (
+    <>
+      <HeroHome />
+      <StatsSection />
+      <AboutSection />
+      <SkillsSection />
+    </>
+  );
+}
+
+export default HomePageClient;
