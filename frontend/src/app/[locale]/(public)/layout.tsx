e1da40e7f578a7581bc710d7836096ab69eaff83
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-06-20 10:53:34 -0300

    feat: adds public layout to frontend/src/app/[locale]/(public)/

---
+++ frontend/src/app/[locale]/(public)/layout.tsx
@@ -0,0 +1,13 @@
+import { Navbar, Footer } from "@/shared";
+
+function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default PublicLayout;
