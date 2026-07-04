62f4fd247280e6550f17d99e44d8bd28ee11258c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-07-04 11:57:03 -0300

    feat: adds forgot-password page to  frontend/src/app/[locale]/(public)/auth/

---
+++ frontend/src/app/[locale]/(public)/auth/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+function ForgotPasswordPage() {
+  return (
+    <section className="py-16 flex items-center justify-center">
+      <p>ForgotPasswordPage</p>
+    </section>
+  );
+}
+
+export default ForgotPasswordPage;
