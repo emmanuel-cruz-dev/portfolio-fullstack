c8dc1d332502bfea49b0d48fddd91ec30ee428a3
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-07-25 17:54:31 -0300

    feat: modifies createClient in src/lib/supabase/server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,12 +1,13 @@
-import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+
+import { createServerClient } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-export const createClient = (
-  cookieStore: Awaited<ReturnType<typeof cookies>>
-) => {
+export const createClient = async () => {
+  const cookieStore = await cookies();
+
   return createServerClient(supabaseUrl!, supabaseKey!, {
     cookies: {
       getAll() {
