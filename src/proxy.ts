581c2d6fe49152e0a2976351e34c54fd98a104ef
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-08-07 13:29:51 -0300

    fix: modifies matcher in src/proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -9,7 +9,5 @@ export async function proxy(request: NextRequest) {
 export default proxy;
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)"],
 };
