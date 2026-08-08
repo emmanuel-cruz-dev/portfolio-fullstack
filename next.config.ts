8671cd51f0a6fbae5d89eb4b8318ecadb64ced20
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-08-08 18:59:48 -0300

    feat: modifies nextConfig in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+  experimental: {
+    rootParams: true,
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
