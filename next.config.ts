34ed7ba3a09b1872fbedd4c0a2e4ec1326ef85ec
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-08-17 14:40:15 -0300

    feat: removes cache components in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
   experimental: {
     rootParams: true,
     staleTimes: {
