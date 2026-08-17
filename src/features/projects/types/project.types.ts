751f3fbb20e767c9b0798b7963275405e1caf218
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-08-17 13:43:06 -0300

    feat: modifies project types in src/features/projects/types/project.types.ts

--- src/features/projects/types/project.types.ts
+++ src/features/projects/types/project.types.ts
@@ -1,5 +1,23 @@
-export interface Project {
-  title: string;
+import type { Tables, TablesInsert, TablesUpdate } from "@/types";
+import type { Translatable, TranslatableList } from "@/shared";
+
+export type ProjectRaw = Omit<
+  Tables<"projects">,
+  "description" | "highlights" | "challenges"
+> & {
+  description: Translatable;
+  highlights: TranslatableList;
+  challenges: TranslatableList;
+};
+
+export type Project = Omit<
+  ProjectRaw,
+  "description" | "highlights" | "challenges"
+> & {
   description: string;
-  href: string;
-}
+  highlights: string[];
+  challenges: string[];
+};
+
+export type ProjectInsert = TablesInsert<"projects">;
+export type ProjectUpdate = TablesUpdate<"projects">;
